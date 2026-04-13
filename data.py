bc3f613a6b77bfa52360c1de83202f1a3b233744
Jan Borowski <bor.jan.g.2018@gmail.com>
2026-04-13 19:11:05 +0200

    Stop tracking wandb for good, add persistent workers and prefetch factor to data loaders

--- data.py
+++ data.py
@@ -11,7 +11,8 @@ IMAGES_DIR = DATA_DIR / "images"
 ANNOTATIONS_FILE = DATA_DIR / "annotations" / "list.txt"
 IMG_SIZE = (224, 224)
 BATCH_SIZE = 32
-NUM_WORKERS = 8
+NUM_WORKERS = 4
+PREFETCH_FACTOR = 2
 CACHE_DIR = DATA_DIR / "cache"
 
 
@@ -113,18 +114,24 @@ def get_data_loaders(
         shuffle=True,
         num_workers=num_workers,
         pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=PREFETCH_FACTOR
     )
     val_loader = DataLoader(
         PetDataset(val_data, val_transform),
         batch_size=batch_size,
         num_workers=num_workers,
         pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=PREFETCH_FACTOR
     )
     test_loader = DataLoader(
         PetDataset(test_data, val_transform),
         batch_size=batch_size,
         num_workers=num_workers,
         pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=PREFETCH_FACTOR
     )
 
     return train_loader, val_loader, test_loader
